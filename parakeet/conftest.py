49b6e39f5288229194d7d8689867d9d76597ede1
Oleksandr <oleksandrko448@gmail.com>
2023-04-13 03:17:58 +0100

    Initial commit

---
+++ parakeet/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from parakeet.users.models import User
+from parakeet.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
---
+++ ref.md
@@ -0,0 +1,3 @@
+pip install -r requirements/local.txt
+py manage.py makemigrations
+py manage.py migrate
