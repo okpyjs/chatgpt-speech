56ab67e138328c83488085db0d546a94456e6d0e
Oleksandr <oleksandrko448@gmail.com>
2023-04-28 07:18:44 +0100

    add some fields to customuser model

--- parakeet/customuser/admin.py
+++ parakeet/customuser/admin.py
@@ -1,3 +1,10 @@
+from customuser.models import User
 from django.contrib import admin
 
+
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(User, UserAdmin)
